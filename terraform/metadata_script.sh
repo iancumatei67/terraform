5cbadb227a26101583b668c2d8a47a4db3121020
iancumatei67 <127155074+iancumatei67@users.noreply.github.com>
2024-03-20 13:19:43 +0200

    Updated metadata_script.sh

--- terraform/metadata_script.sh
+++ terraform/metadata_script.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-
-# Update package lists
-sudo apt-get update -y
-
-# Install Apache
-sudo apt-get install apache2 -y
-
-# Create a simple HTML file for the website
-echo "<html><head><title>Welcome to My Website</title></head><body><h1>Welcome to My Website</h1><p>This is a simple website hosted on an AWS instance.</p></body></html>" | sudo tee /var/www/html/index.html
-
-# Restart Apache to apply changes
-sudo systemctl restart apache2
-
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
